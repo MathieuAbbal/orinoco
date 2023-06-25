a8823a99e9c8d8956bf7397404e03613910e550b
Mathieu <72860893+MathieuAbbal@users.noreply.github.com>
2023-06-25 14:34:57 +0200

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -35,4 +35,12 @@ app.use('/api/cameras', cameraRoutes);
 app.use('/api/teddies', teddyRoutes);
 app.use('/api/furniture', furnitureRoutes);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mathieuabbal.github.io');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
 module.exports = app;
